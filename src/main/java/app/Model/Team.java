83de6c8eea8eebf4017b5abd928a3b03568b1980
Ruymán García Martín <alu0101408866@ull.edu.es>
2025-01-05 16:31:06 +0000

    Update Team.java
    Creacion de getters y setters y funcion to string

--- src/main/java/app/Model/Team.java
+++ src/main/java/app/Model/Team.java
@@ -14,4 +14,71 @@ public class Team {
         this.id = id;
         this.name = name;
     }
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getGoalsFor() {
+        return goalsFor;
+    }
+
+    public void setGoalsFor(int goalsFor) {
+        this.goalsFor = goalsFor;
+    }
+
+    public int getGoalsAgainst() {
+        return goalsAgainst;
+    }
+
+    public void setGoalsAgainst(int goalsAgainst) {
+        this.goalsAgainst = goalsAgainst;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo: " + name + "\n" +
+                "Partidos ganados: " + wins + "\n" +
+                "Partidos empatados: " + draws + "\n" +
+                "Partidos perdidos: " + losses + "\n" +
+                "Goles a favor: " + goalsFor + "\n" +
+                "Goles en contra: " + goalsAgainst + "\n";
+    }
 }
