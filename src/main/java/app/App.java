201264b4781fa207f87d830dacd8dc13aabc79e7
Ruymán García Martín <alu0101408866@ull.edu.es>
2025-01-12 01:54:15 +0000

    Creacion de la clase LineUpContyroller
    Creacion del controlador de las alineaciones

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,13 +1,21 @@
-package app;
+package app.Controller.Strategies;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import app.Controller.ControllerStrategy;
+import app.Model.FootballAPIService;
+import app.Model.Match;
+import app.Model.MatchNotifier;
+import app.Model.MatchUpdater;
+import app.View.Components.LineUpView;
+import javax.swing.*;
+import java.util.Map;
+
+public class LineUpController implements ControllerStrategy {
+    private LineUpView lineupView;
+    private Map<String, Match> liveMatches;
+    private FootballAPIService footballAPIService = new FootballAPIService(); // Simulación del servicio
+    private MatchNotifier matchNotifier = new MatchNotifier(); // Simulación del servicio
+    private MatchUpdater matchUpdater = new MatchUpdater(footballAPIService, matchNotifier); // Simulación del servicio
+
+    public LineUpController(LineUpView lineupView) {
+        this.lineupView = lineupView;
     }
-}
