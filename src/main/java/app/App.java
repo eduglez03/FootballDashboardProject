e007bbe668e15c42d38f43df6f260e819f908c07
eglezgut03 <alu0101461588@ull.edu.es>
2025-01-13 14:59:15 +0000

    Documentación App.java

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -3,11 +3,18 @@ package app;
 import app.View.MainView;
 import javax.swing.*;
 
+/**
+ * Main class of the application.
+ */
 public class App {
+  /**
+   * Main method of the application.
+   * @param args Command line arguments.
+   */
   public static void main(String[] args) {
-    SwingUtilities.invokeLater(() -> {
-      MainView mainView = new MainView();
-      mainView.setVisible(true);
+    SwingUtilities.invokeLater(() -> { // Run the GUI code on the Event-Dispatch Thread
+      MainView mainView = new MainView(); // Create the main view
+      mainView.setVisible(true); // Show the main view
     });
   }
 }
